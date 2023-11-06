0cfe86d886c595ad9fd8d4231db16d6eb5e68473
Anders Newgard <anne9707@colorado.edu>
2023-11-05 20:12:58 -0500

    jawsdb configuration

--- controllers/exp-routes.js
+++ controllers/exp-routes.js
@@ -3,7 +3,7 @@ const { User, Post, Comment } = require("../models");
 const withAuth = require('../utilities/authenticate.js');
 
 // Get all posts or experiences for the logged in user with associated comments. 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
         console.log(req.session.user_id);
         const postData = await Post.findAll({
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
             },
             include: [
                 {
-                    model: User,
+                    model: User,   
                     attributes: ['username'],
                 },
                 {
@@ -29,14 +29,15 @@ router.get('/', async (req, res) => {
         const posts = postData.map((post) => post.get({ plain: true }));
 
        console.log(posts);
-        res.render("all-post", {layout : "experiences", posts});
+        res.render("all-post",
+        {layout : "experiences", posts});
     } catch (err) {
-        res.status(500).json(err);
+        res.redirect('login');
     }
 });
 
 // GET /new route for creating a new post or experience.
-router.get('/new', async (req, res) => {
+router.get('/new', withAuth, async (req, res) => {
     try {
         res.render("new-post", { layout: "experiences" });
     } catch (err) {
@@ -45,7 +46,7 @@ router.get('/new', async (req, res) => {
 });
 
 // GET method for editing a single post or experience with its associated comments and users.
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
             include: [
