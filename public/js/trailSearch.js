c81cad9f79be0079b40cc1997f693ac6a5c06eb8
Chris A <christaehyunahn@gmail.com>
2023-11-06 16:52:20 -0500

    got the array of api call the log in the console, still have trouble rendering it into trails.handlebars

--- public/js/trailSearch.js
+++ public/js/trailSearch.js
@@ -1,17 +1,25 @@
 document.getElementById("searchTrailsButton").addEventListener("click", async function() {
     const searchTerm = document.getElementById("trailSearchInput").value;
-    
+
     try {
         const response = await fetch(`/api/searchTrails?q=${searchTerm}`);
-        const trails = await response.json();
-        
+        const trails = await response.json(); 
+
+        console.log("API Data:", trails);
+
         // Compile the Handlebars template
         const source = document.getElementById("trails-template").innerHTML;
         const template = Handlebars.compile(source);
-    
+
         // Render the results
         const rendered = template({ trails: trails });
         document.getElementById("searchResults").innerHTML = rendered;
+
+        // Debugging logs
+        console.log("Attempting to search for:", searchTerm);
+        console.log("Rendered HTML:", rendered);
+        console.log("API Response:", response);
+
     } catch (error) {
         console.error('Error fetching and displaying trails:', error);
     }
---
+++ utilities/trailService.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const API_ENDPOINT = 'https://data.townofcary.org/api/explore/v2.1/catalog/datasets/greenway-trails/records?limit=20';  
+
+const searchTrails = async (query, pageNumber = 1) => {
+    try {
+        const response = await axios.get(API_ENDPOINT, {
+            params: {
+                q: query,
+                page: pageNumber
+            }
+        });
+
+
+        console.log("API Response:", response.data);
+        return response.data.results || [];
+
+    } catch (error) {
+        throw new Error(`Failed to fetch trails: ${error.message}`);
+    }
+};
+
+module.exports = { searchTrails };
