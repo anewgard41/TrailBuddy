f948da13d6b8ac964aa1bcac97dece1efccb69d3
Chris A <christaehyunahn@gmail.com>
2023-11-06 22:11:03 -0500

    got them to populategit add .

--- public/js/trailSearch.js
+++ public/js/trailSearch.js
@@ -6,6 +6,13 @@ document.getElementById("searchTrailsButton").addEventListener("click", async fu
         const trails = await response.json(); 
         
         console.log("API Data:", trails);
+        
+        const searchResults = document.getElementById("searchResults")
+        trails.forEach(element => {
+            const trailEl = document.createElement("li")
+            trailEl.textContent = element.name;
+            searchResults.appendChild(trailEl);
+        });
 
         // Compile the Handlebars template
         const source = document.getElementById("trails-template").innerHTML;
@@ -16,7 +23,7 @@ document.getElementById("searchTrailsButton").addEventListener("click", async fu
         const rendered = template({ trails: trails });
         console.log("rendered before DOM insert", rendered)
         // Populate the search results into the 'searchResults' div
-        document.getElementById("searchResults").innerHTML = rendered;
+        // document.getElementById("searchResults").innerHTML = rendered;
 
         // Debugging logs
         console.log("Attempting to search for:", searchTerm);
@@ -24,7 +31,7 @@ document.getElementById("searchTrailsButton").addEventListener("click", async fu
         console.log("API Response:", response);
         
         
-
+        // document.location.reload();
     } catch (error) {
         console.error('Error fetching and displaying trails:', error);
     }
